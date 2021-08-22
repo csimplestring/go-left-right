f59716afdb83456aaaac967f1f9cbcd84bcd13fd
Yi Wang <yi.wang@rtl.nl>
2021-08-22 13:37:35 +0200

    use long adder indicator to reduce reader ccontention

--- primitive/read_indicator.go
+++ primitive/read_indicator.go
@@ -1,15 +1,49 @@
 package primitive
 
-import "sync/atomic"
+import (
+	"sync/atomic"
 
-// readIndicator uses an int32 as the counter to count the readers
-type readIndicator struct {
+	g "github.com/linxGnu/go-adder"
+)
+
+type ReadIndicator interface {
+	arrive()
+	depart()
+	isEmpty() bool
+}
+
+type distributedAtomicReadIndicator struct {
+	ingress g.LongAdder
+	egress  g.LongAdder
+}
+
+func newDistributedAtomicReadIndicator() *distributedAtomicReadIndicator {
+	return &distributedAtomicReadIndicator{
+		ingress: g.NewJDKAdder(),
+		egress:  g.NewJDKAdder(),
+	}
+}
+
+func (d *distributedAtomicReadIndicator) arrive() {
+	d.ingress.Inc()
+}
+
+func (d *distributedAtomicReadIndicator) depart() {
+	d.egress.Inc()
+}
+
+func (d *distributedAtomicReadIndicator) isEmpty() bool {
+	return d.egress.Sum() == d.ingress.Sum()
+}
+
+// atomicReadIndicator uses an int32 as the counter to count the readers
+type atomicReadIndicator struct {
 	count *int32
 }
 
-// newReadIndicator creates a readIndicator
-func newReadIndicator() *readIndicator {
-	r := &readIndicator{
+// newAtomicReadIndicator creates a readIndicator
+func newAtomicReadIndicator() *atomicReadIndicator {
+	r := &atomicReadIndicator{
 		count: new(int32),
 	}
 	*r.count = 0
@@ -17,16 +51,16 @@ func newReadIndicator() *readIndicator {
 }
 
 // arrive should be called by the reader goroutine when start reading, increments the counter
-func (r *readIndicator) arrive() {
+func (r *atomicReadIndicator) arrive() {
 	atomic.AddInt32(r.count, 1)
 }
 
 // depart should be called by the reader goroutine when finish reading, decrements the counter
-func (r *readIndicator) depart() {
+func (r *atomicReadIndicator) depart() {
 	atomic.AddInt32(r.count, -1)
 }
 
 // isEmpty returns true if no readers
-func (r *readIndicator) isEmpty() bool {
+func (r *atomicReadIndicator) isEmpty() bool {
 	return atomic.LoadInt32(r.count) == 0
 }
